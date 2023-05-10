0235c9caf89470c92da55341b558307d4942217a
hzgge1 <hzgge1@iscte-iul.pt>
2023-05-10 15:39:12 +0100

    heng > bruno

--- forum/urls.py
+++ forum/urls.py
@@ -3,20 +3,21 @@ from . import views
 
 app_name = 'forum'
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("about", views.about, name="about"),
+    path("", views.index, name="index"), # pagina inicial/home
+    path("about", views.about, name="about"), # pagina about
 
-    path("login", views.loginView, name="loginView"),
-    path("logout", views.logoutView, name="logoutView"),
-    path("sign_up", views.sign_up, name="sign_up"),
-    path("editar_perfil", views.editar_perfil, name='editar_perfil'),
+    path("login", views.loginView, name="loginView"), # pagina login / funcao login -> pagina index
+    path("logout", views.logoutView, name="logoutView"), # funcao logout -> pagina index
+    path("sign_up", views.sign_up, name="sign_up"), # pagina signup / funcao registar -> pagina login
+    path("editar_perfil", views.editar_perfil, name='editar_perfil'), # pagina editar perfil / funcao editar perfil -> pagina perfil
 
-    path("profile", views.profile, name="profile"),
-    path("criar_questao", views.criar_questao, name="criar_questao"),
-    path("pesquisa_questoes", views.pesquisa_questoes, name="pesquisa_questoes"),
-    path("lista_questoes", views.lista_questoes, name="lista_questoes"),
+    path("profile", views.profile, name="profile"), # pagina perfil
+    path("criar_questao", views.criar_questao, name="criar_questao"), # pagina criar questao / funcao criar questao -> pagina index
+    path("pesquisa_questoes", views.pesquisa_questoes, name="pesquisa_questoes"), # funcao pesquisa questoes -> pagina lista questoes
+    path("lista_questoes", views.lista_questoes, name="lista_questoes"), # pagina lista questoes
+    path("questoes_user", views.questoes_user, name="quesotes_user"), # funcao questoes user -> pagina lista questoes
 
-    path("<int:questao_id>/detalhe_questao", views.detalhe_questao, name='detalhe_questao'),
-    path("<int:questao_id>/apagar_questao", views.apagar_questao, name='apagar_questao'),
-    path("<int:questao_id>/like_questao", views.like_questao, name='like_questao'),
+    path("<int:questao_id>/detalhe_questao", views.detalhe_questao, name='detalhe_questao'), # pagina detalhe questao
+    path("<int:questao_id>/apagar_questao", views.apagar_questao, name='apagar_questao'), # funcao apagar questao
+    path("<int:questao_id>/like_questao", views.like_questao, name='like_questao'), # funcao da like a questao
 ]
